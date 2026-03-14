ffaf3edc109ae9cc1de4ac74090bf10ac94302f2
guntas-13 <guntassingh.saran@iitgn.ac.in>
2026-03-14 21:48:11 +0530

    add exit codes for error handling

---
+++ build/include/common/exitcodes.hpp
@@ -0,0 +1,11 @@
+#ifndef COMMON_EXITCODES_HPP
+#define COMMON_EXITCODES_HPP
+
+namespace exitcode 
+{
+    constexpr int SUCCESS = 0;
+    constexpr int PROTOCOL_ERROR = 63;
+    constexpr int OTHER_ERROR = 255;
+}
+
+#endif // COMMON_EXITCODES_HPP
